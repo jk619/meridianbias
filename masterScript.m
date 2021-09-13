c1fe55b7f6f85607bfc195c8ae4eb12db6ede12f
Jan Kurzawski <jk7127@deathvalley.psych.nyu.edu>
2021-09-13 15:06:03 -0400

    new

--- masterScript.m
+++ masterScript.m
@@ -2,6 +2,10 @@ clear
 clc
 close all
 
+rootPath=ebsrootPath();
+cd(rootPath);
+
+
 
 % You can run download_HCP.sh to download the data, otherwise the script
 % will ask you if you want matlab to do it for you.
@@ -15,11 +19,19 @@ else
     if strcmp(str,'Y') || strcmp(str,'y')
         
         % downloads pRF maps for for each subject in HCP dataset (n=181).
+        
+        disp('Downloading HCP 7T retinotopy data - might take a while')
+        websave('./prfresultsmgz.zip', 'https://osf.io/tqprz/download');
+        unzip('prfresultsmgz.zip')
+
         % downloads Benson's atlas to locate V1 ROI.
-        % clones repisotory of vistasoft needed to load mgz files.
-        system('sh download_HCP.sh')
+
+        mkdir('benson_atlas')
+        websave('./benson_atlas/lh.varea.mgz','https://github.com/noahbenson/neuropythy/raw/master/neuropythy/lib/data/fsaverage/surf/lh.benson14_varea.v4_0.mgz');
+        websave('./benson_atlas/rh.varea.mgz','https://github.com/noahbenson/neuropythy/raw/master/neuropythy/lib/data/fsaverage/surf/rh.benson14_varea.v4_0.mgz');
+
         
-        
+        % download MRIr
     elseif strcmp(str,'N') ||  strcmp(str,'n')
         
         error('You need the data!')
@@ -32,8 +44,6 @@ else
     
 end
 
-% We need vistasoft to be able to load mgz surface files.
-addpath(genpath('vistasoft'))
 % reproduce asymmetry for HCP dataset in Figure 3E.
 Fig3E
 % reproduce incremental asymmetry for HCP in Figure 4C.

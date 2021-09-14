85fbe503502834497aa87be6e76d48a440db9b13
Jan Kurzawski <jk7127@deathvalley.psych.nyu.edu>
2021-09-14 10:59:23 -0400

    new

--- masterScript.m
+++ masterScript.m
@@ -5,20 +5,22 @@ close all
 rootPath=ebsrootPath();
 cd(rootPath);
 
+% check dependencies
 
+if ~exist('MRIread.m','file')
+    error('You need freesurfer''s matlab toolbox')
+end
 
-% You can run download_HCP.sh to download the data, otherwise the script
-% will ask you if you want matlab to do it for you.
-
-if exist('prfresultsmgz','dir')
+if exist('prfresultsmgz','dir') && exist('benson_atlas','dir')
     
 else
     
-    str = input('Do you want to download HCP pRF solutions? type Y/N and press return. ','s');
+    str = input('Do you want to download HCP pRF solutions and Benson''s V1 definition. Type Y/N and press return. ','s');
     
-    if strcmp(str,'Y') || strcmp(str,'y')
+    if strcmp(str,'Y') || strcmp(str,'y') || strcmp(str,'yes') || strcmp(str,'Yes')
         
         % downloads pRF maps for for each subject in HCP dataset (n=181).
+        
         
         disp('Downloading HCP 7T retinotopy data - might take a while')
         websave('./prfresultsmgz.zip', 'https://osf.io/tqprz/download');
@@ -27,11 +29,11 @@ else
         % downloads Benson's atlas to locate V1 ROI.
 
         mkdir('benson_atlas')
+        
         websave('./benson_atlas/lh.varea.mgz','https://github.com/noahbenson/neuropythy/raw/master/neuropythy/lib/data/fsaverage/surf/lh.benson14_varea.v4_0.mgz');
         websave('./benson_atlas/rh.varea.mgz','https://github.com/noahbenson/neuropythy/raw/master/neuropythy/lib/data/fsaverage/surf/rh.benson14_varea.v4_0.mgz');
 
         
-        % download MRIr
     elseif strcmp(str,'N') ||  strcmp(str,'n')
         
         error('You need the data!')
